d2490eadae5d56fb7203e470c15da2c733a2964f
pz <2912150017@qq.com>
2024-07-15 22:31:37 +0800

    a

--- boolean/sql_builder.go
+++ boolean/sql_builder.go
@@ -28,8 +28,35 @@ func Switch(f *sqlbuilder.Field) {
 				return enum.Key, nil
 			}
 		}
-		err := errors.Errorf("not foun reversed enum key ;current:%v", in)
+		err := errors.Errorf("not found reversed enum key ;current:%v", in)
 		return nil, err
 	})
+}
 
+// TrunOff  改成false
+func TrunOff(f *sqlbuilder.Field) {
+	f.ValueFns.InsertAsSecond(func(in any) (any, error) {
+		enums := f.Schema.Enums
+		for _, enum := range enums {
+			if enum.Tag == sqlbuilder.Enum_tag_false {
+				return enum.Key, nil
+			}
+		}
+		err := errors.Errorf("not found fase enum key enums:%s", enums.String())
+		return nil, err
+	})
+}
+
+// TrunOn  改成true
+func TrunOn(f *sqlbuilder.Field) {
+	f.ValueFns.InsertAsSecond(func(in any) (any, error) {
+		enums := f.Schema.Enums
+		for _, enum := range enums {
+			if enum.Tag == sqlbuilder.Enum_tag_true {
+				return enum.Key, nil
+			}
+		}
+		err := errors.Errorf("not found true enum key enums:%s", enums.String())
+		return nil, err
+	})
 }
